819dbdc4b41149eb2e732ef0561e24adb5845818
Sergio Bruni <sbruni@socialh4ck.com>
2016-06-21 00:34:23 -0400

    added hotsbuilds json, develop build structure message for bot

--- src/commands/build.js
+++ src/commands/build.js
@@ -26,15 +26,41 @@ class Hero extends Command {
 
 		request({
 			"method" : "GET",
-			"url" : `https://raw.githubusercontent.com/ESWAT/stormchaser/master/data/herotalentbuilds.json`,
+			"url" : `https://hotsbuildsjson.herokuapp.com/hotsbuilds`,
 			"json" : true
 		}, (error, response, body) => {
 			if (error) console.log("[Build]", error);
 
 			else {
-				let each = _.forEach(body, function(value, key) {
+				//body.builds['Sonya'];
+				var heroname = params.join(' ');
+				if (_.isUndefined(body.builds[heroname]))
+					global.bot.client.sendMessage(message, `No results found for "${params.join(' ')}".`);
+				else {
+					var builds = body.builds[heroname]
+					
+					var result = `\n${heroname}`;
+					result += `\n=======================================`;
+					result += `\nLv01: ${builds.build1.lv01}`;
+					result += `\nLv04: ${builds.build1.lv04}`;
+					result += `\nLv07: ${builds.build1.lv07}`;
+					result += `\nLv10: ${builds.build1.lv10}`;
+					result += `\nLv13: ${builds.build1.lv13}`;
+					result += `\nLv16: ${builds.build1.lv16}`;
+					result += `\nLv20: ${builds.build1.lv20}`;
+					result += `\n=======================================`;
+					result += `\nLv01: ${builds.build2.lv01}`;
+					result += `\nLv04: ${builds.build2.lv04}`;
+					result += `\nLv07: ${builds.build2.lv07}`;
+					result += `\nLv10: ${builds.build2.lv10}`;
+					result += `\nLv13: ${builds.build2.lv13}`;
+					result += `\nLv16: ${builds.build2.lv16}`;
+					result += `\nLv20: ${builds.build2.lv20}`;			
+					global.bot.client.sendMessage(message, result);		
+				}
+				/*let each = _.forEach(body, function(value, key) {
 					console.log('value ' , value);
-				});
+				});*/
 			}
 			/*else {
 
