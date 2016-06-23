5087f7afb4b18109a13315fc4dc9c2ba76467608
Sergio Bruni <sbruni@socialh4ck.com>
2016-06-23 07:53:37 -0400

    clean code

--- src/commands/build.js
+++ src/commands/build.js
@@ -32,7 +32,6 @@ class Hero extends Command {
 			if (error) console.log("[Build]", error);
 
 			else {
-				//body.builds['Sonya'];
 				var heroname = params.join(' ');
 				if (_.isUndefined(body.builds[heroname]))
 					global.bot.client.sendMessage(message, `No results found for "${params.join(' ')}".`);
@@ -58,39 +57,9 @@ class Hero extends Command {
 					result += `\nLv20: ${builds.build2.lv20}`;			
 					global.bot.client.sendMessage(message, result);		
 				}
-				/*let each = _.forEach(body, function(value, key) {
-					console.log('value ' , value);
-				});*/
+
 			}
-			/*else {
-
-				let count = 0;
-				let flag = false;
-				let result = '';
-				let each = _.forEach(body, function(value, key) {
-					count++;
-					if (value.name === params.join(' ')){
-						flag = true;
-				  		console.log(value);
-				  		console.log(value.abilities[value.id]);
-				  		
-						result += `\n${value.name}`;
-						result += `\nHP: ${value.stats[value.id].hp} :heart: /MP: ${value.stats[value.id].mana} :droplet:`;
-						result += `\nAbilities:`;
-						_.forEach(value.abilities[value.id], function(value_sk, key) {
-							result += `\n**${value_sk.name}:** ${value_sk.description}`;
-							if (!value_sk.trait){
-								result += `\n 	Mana Cost: ${value_sk.manaCost}`;
-								result += `\n 	CD: ⏰ ${value_sk.cooldown} secs`;								
-							}
-						});
-					}
-					if (count === body.length && flag) global.bot.client.sendMessage(message, result);
-					else if (count === body.length && !flag) global.bot.client.sendMessage(message, `No results found for "${params.join(' ')}".`);
-				});
-			}*/
 		});
-
     }
 
 }
