63d3fc908e969776e535c509a994c4861e0f76fd
Sergio Bruni <sbruni@socialh4ck.com>
2016-06-27 08:01:18 -0400

    added some config vars

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 var config = {};
 
+config.game= process.env.GAME;
 config.email = process.env.EMAIL;
 config.password=  process.env.PASS;
 config.commandPrefix= process.env.COMMPREFIX;
+config.updateChannel= process.env.UPCHANNEL;
+config.newsEnabled= true;
+config.newsLatest= '';
 
 module.exports = config;
