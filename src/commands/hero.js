c9765acb91382010cf5f61f76a2ceccc2cf405d2
Sergio Bruni <sbruni@socialh4ck.com>
2016-06-28 01:42:29 -0400

    added heroes list command

--- src/commands/hero.js
+++ src/commands/hero.js
@@ -22,43 +22,55 @@ class Hero extends Command {
 
     execute(message) {
 
-        let params = this.getParams(message);
+      let params = this.getParams(message);
+      console.log('params ' , params);
 
-		request({
-			"method" : "GET",
-			"url" : `http://heroesjson.com/heroes.json`,
-			"json" : true
-		}, (error, response, body) => {
-			if (error) console.log("[Hero]", error);
-			else {
+      if ( params.join(' ') === 'list') {
+        let count = 0;
+        let result = '```';
+        let each = _.forEach(global.bot.config.hotsList, function(value, key) {
+          count++;
+          result += `\n${value}`;
+          if (count === global.bot.config.hotsList.length) {
+            result += '```';
+            global.bot.client.sendMessage(message, result);
+          }
+        });
+      }
+      else {
+    		request({
+    			"method" : "GET",
+    			"url" : `http://heroesjson.com/heroes.json`,
+    			"json" : true
+    		}, (error, response, body) => {
+    			if (error) console.log("[Hero]", error);
+    			else {
 
-				let count = 0;
-				let flag = false;
-				let result = '';
-				let each = _.forEach(body, function(value, key) {
-					count++;
-					if (value.name === params.join(' ')){
-						flag = true;
-				  		//console.log(value);
-				  		//console.log(value.abilities[value.id]);
-				  		
-						result += `\n${value.name}`;
-						result += `\nHP: ${value.stats[value.id].hp} :heart: /MP: ${value.stats[value.id].mana} :droplet:`;
-						result += `\nAbilities:`;
-						_.forEach(value.abilities[value.id], function(value_sk, key) {
-							result += `\n**${value_sk.name}:** ${value_sk.description}`;
-							if (!value_sk.trait){
-								result += `\n 	Mana Cost: ${value_sk.manaCost}`;
-								result += `\n 	CD: ⏰ ${value_sk.cooldown} secs`;								
-							}
-						});
-					}
-					if (count === body.length && flag) global.bot.client.sendMessage(message, result);
-					else if (count === body.length && !flag) global.bot.client.sendMessage(message, `No results found for "${params.join(' ')}".`);
-				});
-			}
-		});
-
+    				let count = 0;
+    				let flag = false;
+    				let result = '';
+    				let each = _.forEach(body, function(value, key) {
+    					count++;
+    					if (value.name === params.join(' ')){
+    						flag = true;
+    				  		
+    						result += `\n${value.name}`;
+    						result += `\nHP: ${value.stats[value.id].hp} :heart: /MP: ${value.stats[value.id].mana} :droplet:`;
+    						result += `\nAbilities:`;
+    						_.forEach(value.abilities[value.id], function(value_sk, key) {
+    							result += `\n**${value_sk.name}:** ${value_sk.description}`;
+    							if (!value_sk.trait){
+    								result += `\n 	Mana Cost: ${value_sk.manaCost}`;
+    								result += `\n 	CD: ⏰ ${value_sk.cooldown} secs`;								
+    							}
+    						});
+    					}
+    					if (count === body.length && flag) global.bot.client.sendMessage(message, result);
+    					else if (count === body.length && !flag) global.bot.client.sendMessage(message, `No results found for "${params.join(' ')}".`);
+    				});
+    			}
+    		});
+      }
     }
 
 }
